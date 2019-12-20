0c6929fcf8e21149cf5e8486bd7d6672a4e86d04
Vladislav Tupikin <mrrefactoring@yandex.ru>
2019-12-20 18:28:53 +0300

    myself api added

--- src/api/myself.ts
+++ src/api/myself.ts
@@ -1,5 +1,108 @@
 import { Sender } from '../sender';
+import { Callback } from '../callback';
+import { AxiosRequestConfig } from 'axios';
 
 export class Myself {
   constructor(private readonly client: Sender) { }
+
+  public async getPreference(
+    params: {
+      key: string;
+    },
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '/rest/api/2/mypreferences',
+      method: 'GET',
+      params,
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async setPreference(
+    params: {
+      key: string;
+      body: any;
+    },
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '/rest/api/2/mypreferences',
+      method: 'PUT',
+      params: { key: params.key, },
+      data: params.body,
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async deletePreference(
+    params: {
+      key: string;
+    },
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '/rest/api/2/mypreferences',
+      method: 'DELETE',
+      params,
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async getLocale(
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '/rest/api/2/mypreferences/locale',
+      method: 'GET',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  /** @deprecated */
+  public async setLocale(
+    params: {
+      locale: string;
+    },
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '/rest/api/2/mypreferences/locale',
+      method: 'PUT',
+      data: params,
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  /** @deprecated */
+  public async deleteLocale(
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '/rest/api/2/mypreferences/locale',
+      method: 'DELETE',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async getCurrentUser(
+    params?: {
+      expand?: string;
+    },
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '/rest/api/2/myself',
+      method: 'GET',
+      params,
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
 }

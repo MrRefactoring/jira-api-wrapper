d3482ee75aa666cf5c4946c38ec73b5ad651b045
Vladislav Tupikin <mrreafactorng@yandex.ru>
2019-12-25 13:19:31 +0300

    user api added part I

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,5 +1,154 @@
 import { Sender } from '../sender';
+import { AxiosRequestConfig } from 'axios';
+import { Callback } from '../callback';
 
 export class Users {
   constructor(private readonly client: Sender) { }
+
+  public async get(
+    params: {
+      accountId?: string;
+
+    },
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '/rest/api/3/user',
+      method: 'GET',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async create(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async delete(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async bulkGetUsers(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async getAccountIDs(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async getDefaultColumns(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async setDefaultColumns(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async resetDefaultColumns(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async getEmail(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async getEmailBulk(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async getGroups(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
+
+  public async getAll(
+    params: {},
+    callback?: Callback,
+  ): Promise<any> {
+    const request: AxiosRequestConfig = {
+      url: '',
+      method: '',
+    };
+
+    return this.client.sendRequest(request, callback);
+  }
 }
